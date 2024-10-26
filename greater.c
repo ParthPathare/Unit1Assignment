e6158b2e0156bacfe282e75228bf637f4aac24e5
ParthPathare <parthpathare2912@gmail.com>
2024-10-26 22:10:47 +0530

    greater.c

--- greater.c
+++ greater.c
@@ -4,13 +4,13 @@
 
 int main() 
 {
-    int num1, num2;
+    int num1, num2; //Declare num and num2 in int "SUM WILL NOT BE DECLARED" 
     num1 = 100;
-    num2 = 20;
+    num2 = 20; //Give the values to num1&num2
     if (num1 > num2) 
     {
-        printf("%d is greater than %d\n", num1, num2);
-    } 
+        printf("%d is greater than %d\n", num1, num2);   // Here we will use the if else statement so if num1 is greater than num2 then it will generate
+    }                                                    // 100 is greate than 20
     else
     {
         printf("%d is less than %d\n", num1, num2);
