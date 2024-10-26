9f418e08c14aee9b7fb0777be99c5c11931597da
ParthPathare <parthpathare2912@gmail.com>
2024-10-26 21:56:08 +0530

    div.c

--- div.c
+++ div.c
@@ -3,10 +3,10 @@
 #include<stdio.h>
 int main()
 {
-    int num1,num2,sum;
+    int num1,num2,sum;  //Declare num1,num2,sum
     num1=100;
-    num2=20;
-    sum=num1/num2;
+    num2=20;   //Give values to num1&num2
+    sum=num1/num2; // This will give the quotient
     printf("%d",sum);
     return 0;
 }
