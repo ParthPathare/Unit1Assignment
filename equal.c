0b1846ee95ecfa848a3701c8c716ffdf5bb468dd
ParthPathare <parthpathare2912@gmail.com>
2024-10-26 22:02:06 +0530

    equal.c

--- equal.c
+++ equal.c
@@ -4,16 +4,17 @@
 
 int main() 
 {
-    int num1, num2;
+    int num1, num2; //Declare the num1,num2,sum
     num1 = 100;
-    num2 = 20;
+    num2 = 20;   //Give values to num1&num2
     if (num1 == num2) 
     {
-        printf("%d is equal %d\n", num1, num2);
+        printf("%d is equal %d\n", num1, num2);    //Here we will use if else statement so that it can check the
+                                                   // condition and give the output
     } 
     else
     {
-        printf("%d is greater than or less than %d\n", num1, num2);
+        printf("%d is not equal %d\n", num1, num2);
     }
 
     return 0;
