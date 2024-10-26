1a5dd0b0762730ba742eb6ef9281dd83271fb47e
ParthPathare <parthpathare2912@gmail.com>
2024-10-26 21:48:13 +0530

    add.c

--- add.c
+++ add.c
@@ -3,10 +3,10 @@
 #include<stdio.h>
 int main()
 {
-    int num1,num2,sum;
-    num1=10;
-    num2=20;
-    sum=num1+num2;
+    int num1,num2,sum; //Declare num1,num2,sum
+    num1=45;
+    num2=71;
+    sum=num1+num2; // This will add the values given in num1 & num2
     printf("%d",sum);
-    return 0;
+    return 0; //This will return the program
 }
