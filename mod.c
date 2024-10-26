e0c8fe4cf32111c25464b4b41ac138b377ad58d6
ParthPathare <parthpathare2912@gmail.com>
2024-10-26 22:28:37 +0530

    mod.c

--- mod.c
+++ mod.c
@@ -3,10 +3,10 @@
 #include<stdio.h>
 int main()
 {
-    int num1,num2,sum;
-    num1=100;
-    num2=20;
-    sum=num1%num2;
+    int num1,num2,sum; //Declare num1,num2,sum
+    num1=150;
+    num2=50;  //Give values to num1&num2
+    sum=num1%num2; // Here it will generate the remainder
     printf("%d",sum);
     return 0;
 }
