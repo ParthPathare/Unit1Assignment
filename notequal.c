13279d57e2389607d3500c27f5b340923e1ca285
ParthPathare <parthpathare2912@gmail.com>
2024-10-26 22:31:38 +0530

    notequal.c

--- notequal.c
+++ notequal.c
@@ -4,14 +4,14 @@
 
 int main() 
 {
-    int num1, num2;
+    int num1, num2; //Declare num1,num2
     num1 = 100;
-    num2 = 20;
-    if (num1 != num2) 
+    num2 = 20; // Give values to num1 and num2
+    if (num1 != num2) //Here it will check if num1==num2 or num1!=num2
     {
         printf("%d is not equal %d\n", num1, num2);
     } 
-    else
+    else //If 1st condition is not satisfied it will use this statement
     {
         printf("%d is greater than or less than %d\n", num1, num2);
     }
