440106d1c7d3eb0f2529b37a5023eb5d8695222c
ParthPathare <parthpathare2912@gmail.com>
2024-10-26 22:29:51 +0530

    multi.c

--- multi.c
+++ multi.c
@@ -3,10 +3,10 @@
 #include<stdio.h>
 int main()
 {
-    int num1,num2,sum;
-    num1=10;
-    num2=20;
-    sum=num1*num2;
+    int num1,num2,sum; //Declare num1,num2,sum
+    num1=54;
+    num2=0;  //Give values to num1&num2
+    sum=num1*num2; // Hereit will multiply num1&num2
     printf("%d",sum);
     return 0;
 }
