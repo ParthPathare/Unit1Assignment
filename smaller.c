bb3ac5c2f040a7375d1bf4faa1a7e02a5b84ac83
ParthPathare <parthpathare2912@gmail.com>
2024-10-26 22:27:33 +0530

    smaller.c

--- smaller.c
+++ smaller.c
@@ -4,14 +4,14 @@
 
 int main() 
 {
-    int num1, num2;
-    num1 = 100;
-    num2 = 200;
-    if (num1 < num2) 
+    int num1, num2; //Declare num1,num2
+    num1 = 150;
+    num2 = 189;
+    if (num1 < num2) //Here it will check that if num1 is less than num2
     {
         printf("%d is less than %d\n", num1, num2);
     } 
-    else
+    else //if the 1st condition is not satisfied
     {
         printf("%d is greater than %d\n", num1, num2);
     }
